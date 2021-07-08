cf08455270e38c367830f6bb62ba5a7993c6a826
Sailesh Nankani <saileshhps@gmail.com>
2021-07-07 21:59:42 -0400

    Add GPU support

--- jina_faiss_retrieval/my_executors.py
+++ jina_faiss_retrieval/my_executors.py
@@ -21,7 +21,7 @@ class FaissIndexer(Executor):  # Simple exact FAISS indexer
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self._docs = DocumentArray()
-        self._index = faiss.IndexFlatL2(384)
+        self._index = faiss.index_cpu_to_all_gpus(faiss.IndexFlatL2(384))
         Path(self.workspace).mkdir(parents=True, exist_ok=True)
 
     @requests(on='/index')
